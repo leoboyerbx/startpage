8c322dbe95dd5ed58090508b0f0ad9429ab4ae77
leoboyerbx <contact@leoboyer.fr>
2021-07-16 19:11:52 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.red,
+      yellow: colors.yellow,
+      yeleo: {
+        light: '#ffea92',
+        DEFAULT: '#fce14b',
+        dark: '#d2b81d'
+      }
+    },
+    boxShadow: {
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      th: '0 0 25px rgba(0, 0, 0, 0.15)',
+      'th-sm': '0 0 10px rgba(0, 0, 0, 0.15)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none'
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+  ]
+}
